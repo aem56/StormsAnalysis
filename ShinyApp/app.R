5e0cc4f42c6f518bacd21e19008dae20955198a3
Aristide Mooyaart <aristide.mooyaart@gmail.com>
2016-04-16 19:22:17 +0100

    Added files via upload

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(rCharts)
 library(dplyr)
+library(googleVis)
 
 StormData<-read.csv("data/StormData.csv")
 
@@ -8,6 +9,11 @@ eventTypes<-unique(as.character(StormData$EVTYPE))
 DataByYear<- function(data, yearStart, yearEnd, events){
         data %>% filter(YEAR>=yearStart, YEAR<=yearEnd, EVTYPE %in% events) %>% 
         group_by(YEAR) %>% summarise_each(funs(sum), Frequency:PropertyDamage)
+}
+
+DataByState<- function(data, yearStart, yearEnd, events){
+        data %>% filter(YEAR>=yearStart, YEAR<=yearEnd, EVTYPE %in% events) %>% 
+        group_by(STATE) %>% summarise_each(funs(sum), Frequency:PropertyDamage)
 }
 
 ui<-fluidPage(
@@ -18,50 +24,72 @@ ui<-fluidPage(
         p("The source code for processing the database and for this webapp is available on my ",
           a("github.", href="https://github.com/aem56/StormsAnalysis")),
         sliderInput("yearRange","Year Range:",min=1950,max=2011,value = c(1990,2011)),
-        uiOutput("evtypeControls"),
-        actionButton(inputId = "clearAll", label = "Clear selection"),
-        actionButton(inputId = "selectAll", label = "Select all")
+        radioButtons('radio','Storm selection type:',list('All','Manual'),selected='All'),
+        conditionalPanel(condition = "input.radio != 'All'",selectInput('evntTypes', 'Storm types:', eventTypes))
     ),
     mainPanel(
-        h4("Number of recorded storm events by year:"),
-        showOutput("plot1",'morris'),
-        h4("Population impact by year:"),
-        showOutput("plot2", 'morris'),
-        h4("Economic impact by year:"),
-        showOutput("plot3",'morris')
+        tabsetPanel(
+            tabPanel(("By year"),
+                h4("Number of recorded storm events by year:"),
+                showOutput("plot1",'morris'),
+                h4("Population impact by year:"),
+                showOutput("plot2", 'morris'),
+                h4("Economic impact by year:"),
+                showOutput("plot3",'morris')
+            ),
+            tabPanel(("By state"),
+                     h4("Number of recorded storm events by state:"),
+                     htmlOutput("plot4"),
+                     h4("Population impact by state:"),
+                     htmlOutput("plot5"),
+                     h4("Economic impact by state:"),
+                     htmlOutput("plot6")
+            )
+        )
     )
 )
 
 server<-shinyServer(
     function(input,output){
         value<-reactiveValues()
-        value$ET<-eventTypes
-        ET<-reactive({input$evntTypes})
-        output$evtypeControls <- renderUI({
-            checkboxGroupInput('evntTypes', 'Storm types:', eventTypes, selected=value$ET)
-        })
+        value$selc<-eventTypes
         observe({
-            if(input$clearAll == 0) return()
-            value$ET <- c()
-        })
-        observe({
-            if(input$selectAll == 0) return()
-            value$ET <- eventTypes
+            if(input$radio == "All"){
+                value$selc<-eventTypes
+            }
+            else{
+                value$selc<-input$evntTypes
+            }
         })
         output$plot1<-renderChart2({
-            data1<-DataByYear(StormData,input$yearRange[1],input$yearRange[2], input$evntTypes)
+            data1<-DataByYear(StormData,input$yearRange[1],input$yearRange[2], value$selc)
             data1$YEAR<-as.character(data1$YEAR)
             mPlot(x="YEAR",y="Frequency",data=data1,type="Line")
         })
         output$plot2<-renderChart2({
-            data2<-DataByYear(StormData,input$yearRange[1],input$yearRange[2], input$evntTypes)
+            data2<-DataByYear(StormData,input$yearRange[1],input$yearRange[2], value$selc)
             data2$YEAR<-as.character(data2$YEAR)
             mPlot(x="YEAR",y=c("Injuries","Deaths"),data=data2,type="Line")
         })
         output$plot3<-renderChart2({
-            data3<-DataByYear(StormData,input$yearRange[1],input$yearRange[2], input$evntTypes)
+            data3<-DataByYear(StormData,input$yearRange[1],input$yearRange[2], value$selc)
             data3$YEAR<-as.character(data3$YEAR)
             mPlot(x="YEAR", y=c("PropertyDamage","CropDamage"), data=data3, type="Line")
+        })
+        output$plot4<-renderGvis({
+            data4<-DataByState(StormData,input$yearRange[1],input$yearRange[2], value$selc)
+            gvisGeoChart(data4, "STATE", "Frequency",options=list(region="US", displayMode="regions",
+                        resolution="provinces",width=600, height=400))
+        })
+        output$plot5<-renderGvis({
+            data5<-DataByState(StormData,input$yearRange[1],input$yearRange[2], value$selc)
+            gvisGeoChart(data5, "STATE", "Deaths","Injuries",options=list(region="US", displayMode="regions", 
+                        resolution="provinces",width=600, height=400))
+        })
+        output$plot6<-renderGvis({
+            data6<-DataByState(StormData,input$yearRange[1],input$yearRange[2], value$selc)
+            gvisGeoChart(data6, "STATE", "PropertyDamage","CropDamage",options=list(region="US", displayMode="regions", 
+                        resolution="provinces",width=600, height=400))
         })
     }
 )
