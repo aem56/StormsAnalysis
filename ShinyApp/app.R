1418ee786626952e010ff6620c20901dc36e7694
Aristide Mooyaart <aristide.mooyaart@gmail.com>
2016-04-17 12:30:32 +0100

    Added files via upload

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -5,7 +5,8 @@ library(googleVis)
 
 StormData<-read.csv("data/StormData.csv")
 
-eventTypes<-unique(as.character(StormData$EVTYPE))
+eventTypes<-sort(unique(as.character(StormData$EVTYPE)))
+states<-sort(unique(as.character(StormData$STATE)))
 DataByYear<- function(data, yearStart, yearEnd, events){
         data %>% filter(YEAR>=yearStart, YEAR<=yearEnd, EVTYPE %in% events) %>% 
         group_by(YEAR) %>% summarise_each(funs(sum), Frequency:PropertyDamage)
@@ -16,10 +17,15 @@ DataByState<- function(data, yearStart, yearEnd, events){
         group_by(STATE) %>% summarise_each(funs(sum), Frequency:PropertyDamage)
 }
 
+PredictionData<- function(data, yearStart, yearEnd, events, ST){
+        data %>% filter(YEAR>=yearStart, YEAR<=yearEnd, EVTYPE %in% events, STATE %in% ST) %>%
+        group_by(YEAR) %>% summarise_each(funs(sum), Frequency:PropertyDamage)
+}
+
 ui<-fluidPage(
     headerPanel("Storm Database Explorer"),
     sidebarPanel(
-        p("Use this webapp to explore the US National Oceanic and Atmospheric Asministration's storm database. 
+        p("Use this web app to explore the US National Oceanic and Atmospheric Asministration's storm database. 
           You can select a year range and storm types below."),
         p("The source code for processing the database and for this webapp is available on my ",
           a("github.", href="https://github.com/aem56/StormsAnalysis")),
@@ -44,6 +50,31 @@ ui<-fluidPage(
                      htmlOutput("plot5"),
                      h4("Economic impact by state:"),
                      htmlOutput("plot6")
+            ),
+            tabPanel(("Storm predictor"),
+                     h5(""),
+                     p("Predict the effects of any or all storm types, in any or all states, for any year. Simply select
+                        the year range you want to base the prediction over and the types of storms you want to predict 
+                        on the left. Then enter in which year and what state you want to predict for below."),
+                     numericInput("predYr", "Year to predict", min="2016", max="2050",value="2016"),
+                     radioButtons('stateRadio','State selection type:',list('All','Manual'),selected='All'),
+                     conditionalPanel(condition = "input.stateRadio != 'All'",selectInput('state', 'Choose state:',
+                                                                                          states)),
+                     h4("Predicted number of deaths"),
+                     verbatimTextOutput("predictionDeaths"),
+                     h4("Predicted number of injuries"),
+                     verbatimTextOutput("predictionInjuries"),
+                     h4("Predicted cost of crop damage"),
+                     verbatimTextOutput("predictionCropDamage"),
+                     h4("Predicted cost of property damage"),
+                     verbatimTextOutput("predictionPropertyDamage"),
+                     br(),
+                     h5("Notes about the predictions:"),
+                     p("These predictions should only be interpreted as a rough guide. The prediction algorithm uses the
+                        'lm' method, which typically only explains about 30-40% of the variability in this data. In some 
+                        cases, the way the data was collected varied by year, leading to some odd trends which the 
+                        prediction algorithm can struggle with. The algorithm will throw up an error when there is no 
+                       data - try searching for tsunami events in Kansas (KS)!")
             )
         )
     )
@@ -59,6 +90,14 @@ server<-shinyServer(
             }
             else{
                 value$selc<-input$evntTypes
+            }
+        })
+        observe({
+            if(input$stateRadio == "All"){
+                value$selcStates<-states
+            }
+            else{
+                value$selcStates<-input$state
             }
         })
         output$plot1<-renderChart2({
@@ -90,6 +129,34 @@ server<-shinyServer(
             data6<-DataByState(StormData,input$yearRange[1],input$yearRange[2], value$selc)
             gvisGeoChart(data6, "STATE", "PropertyDamage","CropDamage",options=list(region="US", displayMode="regions", 
                         resolution="provinces",width=600, height=400))
+        })
+        output$predictionDeaths<-renderPrint({
+            data7<-PredictionData(StormData,input$yearRange[1],input$yearRange[2],value$selc,value$selcStates)
+            fit<-lm(Deaths~YEAR,data=data7)
+            year<-data.frame(YEAR=input$predYr)
+            if(predict(fit,newdata=year)<0){prediction<-0}else{prediction<-predict(fit,newdata=year)}
+            cat(paste("The prediction is ",round(prediction,digits = 0)," deaths.",sep=""))
+        })
+        output$predictionInjuries<-renderPrint({
+            data8<-PredictionData(StormData,input$yearRange[1],input$yearRange[2],value$selc,value$selcStates)
+            fit<-lm(Injuries~YEAR,data=data8)
+            year<-data.frame(YEAR=input$predYr)
+            if(predict(fit,newdata=year)<0){prediction<-0}else{prediction<-predict(fit,newdata=year)}
+            cat(paste("The prediction is ",round(prediction,digits = 0)," injuries.",sep=""))
+        })
+        output$predictionCropDamage<-renderPrint({
+            data9<-PredictionData(StormData,input$yearRange[1],input$yearRange[2],value$selc,value$selcStates)
+            fit<-lm(CropDamage~YEAR,data=data9)
+            year<-data.frame(YEAR=input$predYr)
+            if(predict(fit,newdata=year)<0){prediction<-0}else{prediction<-predict(fit,newdata=year)}
+            cat(paste("The prediction is $",round(prediction,digits = 0)," crop damage.",sep=""))
+        })
+        output$predictionPropertyDamage<-renderPrint({
+            data10<-PredictionData(StormData,input$yearRange[1],input$yearRange[2],value$selc,value$selcStates)
+            fit<-lm(PropertyDamage~YEAR,data=data10)
+            year<-data.frame(YEAR=input$predYr)
+            if(predict(fit,newdata=year)<0){prediction<-0}else{prediction<-predict(fit,newdata=year)}
+            cat(paste("The prediction is $",round(prediction,digits = 0)," property damage.",sep=""))
         })
     }
 )
